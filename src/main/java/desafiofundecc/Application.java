6e96c0a45fcdff52dfcef71b3e016384f6c00985
Lucas Hideki Sekiya N <hidekilu@vivaldi.net>
2021-10-28 11:45:59 -0300

    [updated:] The Main.java file is now named Application.java for better name reasons. [updated:] The build.gradle file is cleaner to build and run with no warning and extra dependecies.

---
+++ src/main/java/desafiofundecc/Application.java
@@ -0,0 +1,11 @@
+package desafiofundecc;
+
+public final class Application {
+    public Application() {
+        System.out.println("program start.");
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+}
